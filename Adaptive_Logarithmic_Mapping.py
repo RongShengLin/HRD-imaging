4ecb58e802d8bb321ba9be1fb7001f947eed80d0
Eric_Shih <ericpgz858@gmail.com>
2023-03-19 21:21:23 +0800

    revise gamma correction

--- Adaptive_Logarithmic_Mapping.py
+++ Adaptive_Logarithmic_Mapping.py
@@ -5,14 +5,26 @@ import math
 import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 
-def gamma_correction(pixel):
-    if pixel <= 0.0031308:
-        return pixel * 12.92
+"""
+add this from Adaptive_Logarithmic_Mapping import * 
+call Adaptive_Logarithmic_Mapping(HDR_image, b = 0.85, L_dmax = 100, gamma_correction_type = 1)
+HDR_image : 需要做 tone mapping 的 image
+b, L_dmax : hyper parameter
+gamma_correction_type : 用哪個方式修正圖片 default = 1 (type 1 比較亮, type 2 比較暗)
+"""
+def gamma_correction(pixel, type):
+    if type == 1:
+        if pixel <= 0.018 : 
+            return 4.5*pixel
+        else:
+            return 1.099*pixel**0.45-0.099
     else :
-        return 1.055 * (pixel**1/2.4) - 0.055
-        #return pixel * 2
+        if pixel <= 0.0031308:
+            return pixel * 12.92
+        else :
+            return 1.055 * (pixel**1/2.4) - 0.055
     
-def Adaptive_Logarithmic_Mapping(HDR_image, b = 0.85, L_dmax = 100):
+def Adaptive_Logarithmic_Mapping(HDR_image, b = 0.85, L_dmax = 100, gamma_correction_type = 1):
     width, height, _ = HDR_image.shape
     CIE_image = cv2.cvtColor(HDR_image, cv2.COLOR_BGR2XYZ)
 
@@ -29,9 +41,9 @@ def Adaptive_Logarithmic_Mapping(HDR_image, b = 0.85, L_dmax = 100):
             Y_w = CIE_image[i][j][1]/L_avg
             L_d = multiplier * math.log(Y_w + 1) * math.log(2 + ((Y_w/L_wmax)**bias) * 8) 
             scale = L_d/CIE_image[i][j][1]
-            LDR_image[i][j][0] = gamma_correction(scale*CIE_image[i][j][0])
-            LDR_image[i][j][1] = gamma_correction(scale*CIE_image[i][j][1])
-            LDR_image[i][j][2] = gamma_correction(scale*CIE_image[i][j][2])
+            LDR_image[i][j][0] = gamma_correction(scale*CIE_image[i][j][0], gamma_correction_type)
+            LDR_image[i][j][1] = gamma_correction(scale*CIE_image[i][j][1], gamma_correction_type)
+            LDR_image[i][j][2] = gamma_correction(scale*CIE_image[i][j][2], gamma_correction_type)
 
     LDR_image = cv2.cvtColor(LDR_image, cv2.COLOR_XYZ2BGR)
     return LDR_image
